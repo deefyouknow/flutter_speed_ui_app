29b9b25213c40cbf9b27e4ebfc4c9483f6a162f7
deefyouknow <deefyouknow@gmail.com>
2025-04-09 20:17:21 +0700

    0.0.1_first_setup

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_speed_ui_app/view/home_ui.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeUi(),
+    );
+  }
+}
